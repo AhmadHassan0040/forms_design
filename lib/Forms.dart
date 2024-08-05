b727192a8f5c9f4ee3b2ff4f0f4e42260d317899
Ahmad Hassan <128656292+AhmadHassan0040@users.noreply.github.com>
2024-08-05 15:20:47 +0500

    Thired Variation

--- lib/Forms.dart
+++ lib/Forms.dart
@@ -28,139 +28,164 @@ class _FormsDesignState extends State<FormsDesign> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              controller: initScroll,
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 60,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Container(
-                      width: 200,
-                      height: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.black,
-                        boxShadow: [
-                          BoxShadow(
-                            color:
-                                Colors.black.withOpacity(0.5), // Shadow color
-                            spreadRadius: 4, // Shadow spread radius
-                            blurRadius: 4, // Shadow blur radius
-                            offset: Offset(7, 10), // Shadow offset
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Container(
-                      width: 200,
-                      height: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.blue,
-                        boxShadow: [
-                          BoxShadow(
-                            color:
-                                Colors.black.withOpacity(0.5), // Shadow color
-                            spreadRadius: 4, // Shadow spread radius
-                            blurRadius: 4, // Shadow blur radius
-                            offset: Offset(7, 10), // Shadow offset
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Container(
-                      width: 200,
-                      height: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.pink,
-                        boxShadow: [
-                          BoxShadow(
-                            color:
-                                Colors.black.withOpacity(0.5), // Shadow color
-                            spreadRadius: 4, // Shadow spread radius
-                            blurRadius: 4, // Shadow blur radius
-                            offset: Offset(7, 10), // Shadow offset
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Container(
-                      width: 200,
-                      height: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.red,
-                        boxShadow: [
-                          BoxShadow(
-                            color:
-                                Colors.black.withOpacity(0.5), // Shadow color
-                            spreadRadius: 4, // Shadow spread radius
-                            blurRadius: 4, // Shadow blur radius
-                            offset: Offset(7, 10), // Shadow offset
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Container(
-                      width: 200,
-                      height: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.yellow,
-                        boxShadow: [
-                          BoxShadow(
-                            color:
-                                Colors.black.withOpacity(0.5), // Shadow color
-                            spreadRadius: 4, // Shadow spread radius
-                            blurRadius: 4, // Shadow blur radius
-                            offset: Offset(7, 10), // Shadow offset
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 60,
-                  ),
-                ],
-              ),
-            ),
-          ],
+      body: Stack(children: [
+        Positioned.fill(
+          child: Image.asset(
+            'lib/Images/bg.jpg',
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'FORM DESIGNS',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40),
+              ),
+              Text(
+                'CHOOSE A DESIGN',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15),
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                controller: initScroll,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 60,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.white54,
+                          boxShadow: [
+                            BoxShadow(
+                              color:
+                                  Colors.black.withOpacity(0.5), // Shadow color
+                              spreadRadius: 4, // Shadow spread radius
+                              blurRadius: 4, // Shadow blur radius
+                              offset: Offset(7, 10), // Shadow offset
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.white54,
+                          boxShadow: [
+                            BoxShadow(
+                              color:
+                                  Colors.black.withOpacity(0.5), // Shadow color
+                              spreadRadius: 4, // Shadow spread radius
+                              blurRadius: 4, // Shadow blur radius
+                              offset: Offset(7, 10), // Shadow offset
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.white54,
+                          boxShadow: [
+                            BoxShadow(
+                              color:
+                                  Colors.black.withOpacity(0.5), // Shadow color
+                              spreadRadius: 4, // Shadow spread radius
+                              blurRadius: 4, // Shadow blur radius
+                              offset: Offset(7, 10), // Shadow offset
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.white54,
+                          boxShadow: [
+                            BoxShadow(
+                              color:
+                                  Colors.black.withOpacity(0.5), // Shadow color
+                              spreadRadius: 4, // Shadow spread radius
+                              blurRadius: 4, // Shadow blur radius
+                              offset: Offset(7, 10), // Shadow offset
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.white54,
+                          boxShadow: [
+                            BoxShadow(
+                              color:
+                                  Colors.black.withOpacity(0.5), // Shadow color
+                              spreadRadius: 4, // Shadow spread radius
+                              blurRadius: 4, // Shadow blur radius
+                              offset: Offset(7, 10), // Shadow offset
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 60,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
