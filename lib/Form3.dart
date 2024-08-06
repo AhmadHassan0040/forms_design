00c758de3536afede78cecda9bc7c1c1522d17b8
Ahmad Hassan <128656292+AhmadHassan0040@users.noreply.github.com>
2024-08-06 11:59:25 +0500

    Fifth Variation

--- lib/Form3.dart
+++ lib/Form3.dart
@@ -2,8 +2,28 @@
 
 import 'package:flutter/material.dart';
 
-class Form3 extends StatelessWidget {
-  const Form3({super.key});
+class Form3 extends StatefulWidget {
+  Form3({super.key});
+
+  @override
+  State<Form3> createState() => _Form3State();
+}
+
+class _Form3State extends State<Form3> {
+  String? selectedValue;
+
+  List<String> options = [
+    '+90',
+    '+91',
+    '+92',
+    '+93',
+    '+94',
+    '+95',
+    '+96',
+    '+97',
+    '+98',
+    '+99'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -13,16 +33,31 @@ class Form3 extends StatelessWidget {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("FORM TEST"),
+          Text(
+            "FORM TEST",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+          ),
+          SizedBox(
+            height: 50,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(
-                width: 10,
+                width: 30,
               ),
               Flexible(
                 child: TextField(
-                  decoration: InputDecoration(hintText: "First Name"),
+                  decoration: InputDecoration(
+                    label: Text('First Name'),
+                    labelStyle:
+                        TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12)),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 5),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
                 ),
               ),
               SizedBox(
@@ -30,11 +65,71 @@ class Form3 extends StatelessWidget {
               ),
               Flexible(
                 child: TextField(
-                  decoration: InputDecoration(hintText: "Second Name"),
+                  decoration: InputDecoration(
+                      label: Text('Second Name'),
+                      labelStyle:
+                          TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 5),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                ),
+              ),
+              SizedBox(
+                width: 30,
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 30,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    color: Colors.red.shade50,
+                    borderRadius: BorderRadius.circular(12)),
+                child: DropdownButton<String>(
+                  borderRadius: BorderRadius.circular(12),
+                  value: selectedValue,
+                  hint: Text('+9..'),
+                  items: options.map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (newValue) {
+                    setState(() {
+                      selectedValue = newValue;
+                    });
+                  },
                 ),
               ),
               SizedBox(
                 width: 10,
+              ),
+              Flexible(
+                child: TextField(
+                  decoration: InputDecoration(
+                      label: Text('Phone No'),
+                      hintText: "0000000000",
+                      labelStyle:
+                          TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 5),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                ),
+              ),
+              SizedBox(
+                width: 30,
               ),
             ],
           )
